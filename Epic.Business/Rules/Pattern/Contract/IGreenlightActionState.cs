7b1de6493ac41f27f9ec5975529b9a5e1269732e
Torres, Alexander <Alexander.Torres@ssa.gov>
2024-11-22 11:20:30 -0600

    initial code as provided by DOE

--- Epic.Business/Rules/Pattern/Contract/IGreenlightActionState.cs
+++
@@ -1,8 +0,0 @@
-using Epic.CrossCutting.DTOs.Models;
-
-namespace Epic.Business.Rules.Pattern.Contract;
-
-public interface IGreenlightActionState
-{
-    void Execute(GreenlightCommsModel model, List<string> updatedProperties);
-}
